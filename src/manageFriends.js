4d94a0f9122d59dd910eb676ca06927daf6b7723
Jennifer Pazos <jmp521@gmail.com>
2021-12-08 17:16:05 -0600

    commit tree d817d213527555bb74283d1ebc103ba1755542f6

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,11 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = {friends: []}, action) {
+  console.log("action", action)
+    switch(action.type) {
+      case "friends/add": 
+        return {...state, friends: [...state.friends, action.payload]}
+      case "friends/remove": 
+        return {...state, friends: state.friends.filter(f => f.id !== action.payload)}
+      default:
+        return state
+    }
 }
